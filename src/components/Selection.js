c32545068cd8941b5583a492021522210a98cef2
Gaurav Soni <gsoni8158@gmail.com>
2023-05-20 19:21:56 +0530

    submitting code-Sat May 20 2023 19:21:55 GMT+0530 (India Standard Time)

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,24 @@
+import React,{useState} from "react";
 
+
+let Selection=({keys,applyColor})=>{
+    console.log(keys,applyColor())
+    console.log(applyColor())
+    function colorChange(e){
+        console.log(e.target)
+        let v=e.target;
+        v.style.backgroundColor=`${applyColor().background}`
+    }
+    return (
+        <div>
+            <div
+           //add style
+        style={{width:"100px"}
+        }
+             className={keys}
+             onClick={colorChange}
+             >hi</div>
+        </div>
+    )
+}
+export default Selection
